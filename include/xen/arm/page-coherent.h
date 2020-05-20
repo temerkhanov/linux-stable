d8c281e30101602e180dde4c07cea42b494a8eb6
Stefano Stabellini <stefano.stabellini@xilinx.com>
2020-05-20 16:43:08 -0700

    xen/arm: call dma_to_phys on the dma_addr_t parameter of dma_cache_maint
    Add a struct device* parameter to dma_cache_maint.
    
    Translate the dma_addr_t parameter of dma_cache_maint by calling
    dma_to_phys. Do it for the first page and all the following pages, in
    case of multipage handling.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- include/xen/arm/page-coherent.h
+++ include/xen/arm/page-coherent.h
@@ -8,12 +8,17 @@
 static inline void *xen_alloc_coherent_pages(struct device *hwdev, size_t size,
 		dma_addr_t *dma_handle, gfp_t flags, unsigned long attrs)
 {
+	void *cpu_addr;
+        if (dma_alloc_from_dev_coherent(hwdev, size, dma_handle, &cpu_addr))
+            return cpu_addr;
 	return dma_direct_alloc(hwdev, size, dma_handle, flags, attrs);
 }
 
 static inline void xen_free_coherent_pages(struct device *hwdev, size_t size,
 		void *cpu_addr, dma_addr_t dma_handle, unsigned long attrs)
 {
+	if (dma_release_from_dev_coherent(hwdev, get_order(size), cpu_addr))
+            return;
 	dma_direct_free(hwdev, size, cpu_addr, dma_handle, attrs);
 }
 
